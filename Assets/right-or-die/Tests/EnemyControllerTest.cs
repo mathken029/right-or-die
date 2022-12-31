4e68d00472d6603bc451f08fbddfc55dfaffd699
@_mathken <11295237+mathken029@users.noreply.github.com>
2022-12-31 13:46:46 +0900

    空のテストを追加しました。

---
+++ Assets/right-or-die/Tests/EnemyControllerTest.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class EnemyControllerTest
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void EnemyControllerTestSimplePasses()
+    {
+        // Use the Assert class to test conditions
+    }
+
+    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // `yield return null;` to skip a frame.
+    [UnityTest]
+    public IEnumerator EnemyControllerTestWithEnumeratorPasses()
+    {
+        // Use the Assert class to test conditions.
+        // Use yield to skip a frame.
+        yield return null;
+    }
+
+    [Test]
+    public void DamageTest()
+    {
+        /*
+        float _enemyHitpoint = 1;
+        EnemyController _enemyController = new EnemyController();
+        float _enemyHitpointDamaged = _enemyController.Damage(_enemyHitpoint);
+        Assert.That(_enemyHitpointDamaged == _enemyHitpoint - 1);
+        */
+    }
+
+}
